29b28ec7e20324e890a6367e7619dddb28852e2e
jj1guj <jj1guj@jj1guj.net>
2024-03-17 23:52:46 +0900

    ASAMを使用するように改造

--- dlshogi/train.py
+++ dlshogi/train.py
@@ -3,6 +3,8 @@ import torch
 import torch.optim as optim
 import torch.nn.functional as F
 from torch.optim.swa_utils import AveragedModel, update_bn
+from dlshogi.sam.sam import SAM
+from dlshogi.sam.example.utility.bypass_bn import disable_running_stats, enable_running_stats
 
 from dlshogi.common import *
 from dlshogi.network.policy_value_network import policy_value_network
@@ -82,11 +84,13 @@ def main(*argv):
 
     if args.optimizer[-1] != ')':
         args.optimizer += '()'
-    optimizer = eval('optim.' + args.optimizer.replace('(', '(model.parameters(),lr=args.lr,' + 'weight_decay=args.weight_decay,' if args.weight_decay >= 0 else ''))
+    # optimizer = eval('optim.' + args.optimizer.replace('(', '(model.parameters(),lr=args.lr,' + 'weight_decay=args.weight_decay,' if args.weight_decay >= 0 else ''))
+    base_optimizer = torch.optim.SGD
+    optimizer = SAM(model.parameters(), base_optimizer, lr=args.lr, momentum=0.9, adaptive=True, rho=2.0)
     if args.lr_scheduler:
         if args.lr_scheduler[-1] != ')':
             args.lr_scheduler += '()'
-        scheduler = eval('optim.lr_scheduler.' + args.lr_scheduler.replace('(', '(optimizer,'))
+        scheduler = eval('optim.lr_scheduler.' + args.lr_scheduler.replace('(', '(base_optimizer,'))
     if args.use_swa:
         logging.info(f'use swa(swa_start_epoch={args.swa_start_epoch}, swa_freq={args.swa_freq}, swa_n_avr={args.swa_n_avr})')
         ema_a = args.swa_n_avr / (args.swa_n_avr + 1)
@@ -249,9 +253,42 @@ def main(*argv):
         for x1, x2, t1, t2, value in train_dataloader:
             t += 1
             steps += 1
+
+            #SAM 1st Path
+            optimizer.step = optimizer.first_step
             with torch.cuda.amp.autocast(enabled=args.use_amp):
                 model.train()
 
+                enable_running_stats(model)
+                y1, y2 = model(x1, x2)
+
+                model.zero_grad()
+                loss1 = cross_entropy_loss_with_soft_target(y1, t1)
+                if args.use_critic:
+                    z = t2.view(-1) - value.view(-1) + 0.5
+                    loss1 = (loss1 * z).mean()
+                else:
+                    loss1 = loss1.mean()
+                if args.beta:
+                    loss1 += args.beta * (F.softmax(y1, dim=1) * F.log_softmax(y1, dim=1)).sum(dim=1).mean()
+                loss2 = bce_with_logits_loss(y2, t2)
+                loss3 = bce_with_logits_loss(y2, value)
+                loss = loss1 + (1 - args.val_lambda) * loss2 + args.val_lambda * loss3
+
+            scaler.scale(loss).backward()
+            if args.clip_grad_max_norm:
+                scaler.unscale_(optimizer)
+                torch.nn.utils.clip_grad_norm_(model.parameters(), args.clip_grad_max_norm)
+            scaler.step(optimizer)
+            scaler.update()
+
+            # SAM 2nd Path
+            optimizer.zero_grad()
+            optimizer.step = optimizer.second_step
+            with torch.cuda.amp.autocast(enabled=args.use_amp):
+                model.train()
+
+                disable_running_stats(model)
                 y1, y2 = model(x1, x2)
 
                 model.zero_grad()
