386c84eb9b5ef0896515467f1b45861de8e60a3e
jj1guj <jj1guj@jj1guj.net>
2024-03-18 00:28:19 +0900

    setup packageにsamが漏れていたため追加

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
     version = '0.1.1',
     author = 'Tadao Yamaoka',
     url='https://github.com/TadaoYamaoka/DeepLearningShogi',
-    packages = ['dlshogi', 'dlshogi.network', 'dlshogi.utils'],
+    packages = ['dlshogi', 'dlshogi.network', 'dlshogi.utils', 'dlshogi.sam'],
     ext_modules=ext_modules,
     cmdclass={'build_ext': my_build_ext},
     description = 'DeepLearningShogi(dlshogi)',
